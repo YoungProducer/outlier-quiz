b1661f414305d015f8fb710fb21793313d773b3d
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-25 12:39:53 +0300

    style: changed name of parse function in utils/parse-questions.js.

--- src/utils/parse-questions.js
+++ src/utils/parse-questions.js
@@ -1,11 +1,11 @@
-module.exports = parse
+module.exports = parseQuestions
 
 /**
  * Function which accept array of questiong
  * and then replace all spec characters to string
  * via 'decodeURIComponent';
  */
-function parse (questions) {
+function parseQuestions (questions) {
   questions.map(question =>
     question.reduce(function (acc, [key, value]) {
       return {
