fdb1c307a935f964b847a30196511935432aa91a
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-25 14:49:02 +0300

    feat: added different states for buttons.

--- src/components/Quiz/Answers/index.js
+++ src/components/Quiz/Answers/index.js
@@ -1,4 +1,5 @@
 const React = require('react')
+const classNames = require('classnames')
 
 const styles = require('./styles.module.css')
 
@@ -12,16 +13,38 @@ function Answers ({
     return [...incorrect, correct]
   }, [correct, incorrect])
 
+  const [selectedAnswer, setSelectedAnswer] = React.useState('')
+
+  function handleButtonClick (answer, isUnselectable) {
+    if (!isUnselectable) {
+      setSelectedAnswer(answer)
+    }
+  }
+
   return (
     <div className={styles.wrapper}>
       {allAnswers.map(function (answer, index) {
+        const isSelected = selectedAnswer === answer
+        const isCorrect =
+          answer === correct &&
+          selectedAnswer !== '' &&
+          !isSelected
+        const isUnselectable =
+          selectedAnswer !== '' &&
+          !isSelected
+
         return (
           <div
             key={index}
             className={styles.buttonWrapper}
           >
             <button
-              className={styles.button}
+              className={classNames(styles.button, {
+                [styles.buttonCorrect]: isCorrect,
+                [styles.buttonSelected]: isSelected,
+                [styles.buttonUnselectable]: isUnselectable
+              })}
+              onClick={() => handleButtonClick(answer, isUnselectable)}
             >
               {answer}
             </button>
